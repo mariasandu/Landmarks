c62a181976d6e9bde6a75bcb3a021fea07a6ecad
Maria Sandu <Maria.Sandu@emids.com>
2023-09-04 13:49:05 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Maria Sandu on 04.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
